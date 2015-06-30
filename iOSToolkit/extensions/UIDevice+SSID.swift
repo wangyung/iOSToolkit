06af8cfff2c5bc36dcbc1745c80ce48b77bc0d38
Freddie Wang <wangyung@gmail.com>
2015-06-30 20:07:54 +0800

    Add UIDevice+SSID

---
+++ iOSToolkit/extensions/UIDevice+SSID.swift
@@ -0,0 +1,31 @@
+//
+//  UIDevice+SSID.swift
+//  iOSToolkit
+//
+//  Created by Freddie Wang on 2015/6/30.
+//  Copyright (c) 2015年 freddie. All rights reserved.
+//
+
+import UIKit
+import SystemConfiguration.CaptiveNetwork
+
+extension UIDevice {
+    public var SSID: String? {
+        get {
+            var currentSSID: String? = nil
+            if let interfaces = CNCopySupportedInterfaces() {
+                let interfacesArray = interfaces.takeRetainedValue() as! [String]
+                if interfacesArray.count > 0 {
+                    let interfaceName = interfacesArray[0] as String
+                    let unsafeInterfaceData = CNCopyCurrentNetworkInfo(interfaceName)
+                    if unsafeInterfaceData != nil {
+                        let interfaceData = unsafeInterfaceData.takeRetainedValue() as Dictionary!
+                        
+                        currentSSID! = interfaceData["SSID"] as! String
+                    }
+                }
+            }
+            return currentSSID
+        }
+    }
+}
